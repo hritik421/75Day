1988a8adf8457f4a9d8e330c8e6569763dcf0b40
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-08 21:16:21 +0530

    Create Bridge edge in a graph.cpp

---
+++ Day2/Bridge edge in a graph.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    private:
+    void dfs(int src, vector<int> adj[], int c, int d,vector<bool>& vis){
+        vis[src]=true;
+        
+        for(int nbr:adj[src]){
+            
+            // We don't want the DFS traversal to consider the edge (c, d) while exploring other paths in the graph.
+            if(nbr==d && src==c) continue;
+            if(!vis[nbr]){
+                dfs(nbr,adj,c,d,vis);
+            }
+        }
+    }
+	public:
+    //Function to find if the given edge is a bridge in graph.
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        // Code here
+        vector<bool> vis(V,false);
+        dfs(c,adj,c,d,vis);
+        if(vis[d]){
+            return false;
+        }
+        return true;
+    }
+};

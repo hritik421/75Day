a563332a21fc27a5feee2db0ca375548279fb1ce
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-10 22:26:15 +0530

    Update BellmanFord.cpp

--- Day4/BellmanFord.cpp
+++ Day4/BellmanFord.cpp
@@ -7,3 +7,42 @@
 // }
 // Why N-1? 
 // Since in a graph of n nodes, in worst case you will take n-1 edges to reach from the first to the last, there by we iterate for n-1 times.  
+
+// User function Template for C++
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Code here
+        vector<int> dist(V,1e8);
+        dist[S]=0;
+        for(int i=0;i<V-1;i++){
+            for(auto edge:edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(dist[v]> (w + dist[u])){
+                    dist[v] = w + dist[u];
+                }
+            }
+        }
+        
+        for(auto edge:edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            if(dist[v]> (w + dist[u])){
+                return {-1};
+            }
+        }
+        
+        return dist;
+    }
+};

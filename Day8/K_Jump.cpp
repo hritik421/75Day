5b9d50ae4d2d0ef5c63a474bfbc1e8845346b843
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-15 11:04:36 +0530

    Create K_Jump.cpp

---
+++ Day8/K_Jump.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  private:
+    int Dp(int n, vector<int>& height, vector<int>& dp){
+        if(n==0) return 0;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        int oneStep = Dp(n-1, height, dp) + abs(height[n] - height[n-1]);
+        int twoStep = INT_MAX;
+        if(n>1){
+            twoStep = Dp(n-2, height, dp) + abs(height[n] - height[n-2]);
+        }
+        
+        return dp[n] = min(oneStep, twoStep);
+    }
+  public:
+    int minimumEnergy(vector<int>& height, int n) {
+        // Code here
+        
+        vector<int> dp(n,0);
+        int k=2;
+        for(int i=1;i<n;i++){
+            int minStep = INT_MAX;
+            for(int j=1;j<=k;j++){
+                if(i-j>=0){
+                    int step = dp[i-j] + abs(height[i]-height[i-j]);
+                    minStep = min(minStep,step);
+                }
+            }
+            dp[i] = minStep;
+        }
+        return dp[n-1];
+    }
+};

d83b9619d99ffe8a9bc83db8d41fc9a185558aca
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-16 22:18:45 +0530

    Update UniquePathWithObstacle.cpp

--- Day9/UniquePathWithObstacle.cpp
+++ Day9/UniquePathWithObstacle.cpp
@@ -34,3 +34,28 @@ class Solution {
         return f(n-1,m-1,grid,dp);
     }
 };
+
+// Tabulation
+
+public:
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                // base condition
+                if(grid[i][j]==0) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = (up + left)%mod;
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }

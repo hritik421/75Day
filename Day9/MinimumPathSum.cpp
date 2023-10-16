552f2afd15282fe0ae6be197629ad8317f7e2510
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-16 23:38:23 +0530

    Update MinimumPathSum.cpp

--- Day9/MinimumPathSum.cpp
+++ Day9/MinimumPathSum.cpp
@@ -21,3 +21,26 @@ int minSumPath(vector<vector<int>> &grid) {
     return f(n-1,m-1,grid,dp);
 }
 
+// Tabulation
+
+int minSumPath(vector<vector<int>> &grid) {
+    // Write your code here.
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dp(n,vector<int>(m,0));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0&&j==0) dp[0][0] = grid[0][0];
+            else{
+                int top=1e9+7,left=1e9+7;
+                if(i>0) top = grid[i][j] + dp[i-1][j];
+                if(j>0) left = grid[i][j] + dp[i][j-1];
+                dp[i][j] = min(top,left);
+            }
+        }
+    }
+    return dp[n-1][m-1];
+}
+
+//

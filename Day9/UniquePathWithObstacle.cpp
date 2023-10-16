40599b7b6bc5e305d12f312e6de1dd23e2713668
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-16 22:22:53 +0530

    Update UniquePathWithObstacle.cpp

--- Day9/UniquePathWithObstacle.cpp
+++ Day9/UniquePathWithObstacle.cpp
@@ -59,3 +59,29 @@ public:
         
         return dp[n-1][m-1];
     }
+
+// Space Optimization
+
+public:
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        vector<int> prev(m,0);
+        
+        for(int i=0;i<n;i++){
+            vector<int> curr(m,0);
+            for(int j=0;j<m;j++){
+                // base condition
+                if(grid[i][j]==0) curr[j]=0;
+                else if(i==0 && j==0) curr[j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = (up + left)%mod;
+                }
+            }
+            prev=curr;
+        }
+        
+        return prev[m-1];
+    }

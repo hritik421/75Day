8da664baa64ce33a598cad60e06aff767a414a54
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-18 23:28:09 +0530

    Update TriangleGrid.cpp

--- Day10/TriangleGrid.cpp
+++ Day10/TriangleGrid.cpp
@@ -19,3 +19,23 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
 	vector<vector<int>> dp(n,vector<int>(n,1e7+7));
 	return f(0,0,n-1,triangle,dp);
 }
+
+// Tabulation
+
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+	vector<vector<int>> dp(n,vector<int>(n,0));
+
+	for(int j=0;j<n;j++) dp[n-1][j] = triangle[n-1][j];
+
+	for(int i=n-2;i>=0;i--){
+		for(int j=i;j>=0;j--){
+			int bottom = triangle[i][j] + dp[i+1][j];
+			int diagnol = triangle[i][j] + dp[i+1][j+1];
+
+			dp[i][j] = min(bottom, diagnol);
+		}
+	}
+
+	return dp[0][0];
+}

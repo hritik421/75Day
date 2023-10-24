88ebb0448e41a293eba86ea8e4431b9dcab28086
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-24 22:24:35 +0530

    Update CherryPickup3D.cpp

--- Day11/CherryPickup3D.cpp
+++ Day11/CherryPickup3D.cpp
@@ -72,3 +72,45 @@ int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
 
     return dp[0][0][c-1];
 }
+
+// Tabulation
+
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+    // Write your code here.
+    vector<vector<int>> front(c,vector<int>(c,0));
+
+    // Base case
+    for(int j1=0;j1<c;j1++){
+        for(int j2=0;j2<c;j2++){
+            if(j1==j2) front[j1][j2] = grid[r-1][j1];
+            else front[j1][j2] = grid[r-1][j1] + grid[r-1][j2];
+        }
+    }
+
+    // 
+
+    for(int i=r-2;i>=0;i--){
+        vector<vector<int>> curr(c,vector<int>(c,0));
+        for(int j1=0;j1<c;j1++){
+            for (int j2 = 0; j2 < c; j2++) {
+                int maxi = -1e8;
+                for(int dj1 = -1; dj1<=1; dj1++){
+                    for(int dj2=-1; dj2<=1; dj2++){
+                        int value = 0;
+                        if(j1==j2) value = grid[i][j1];
+                        else value = grid[i][j1] + grid[i][j2];
+
+                        if(j1+dj1>=0 && j1+dj1<c && j2+dj2>=0 && j2+dj2<c) value += front[j1+dj1][j2+dj2];
+                        else value += -1e8;
+
+                        maxi = max(maxi, value);
+                    }
+                }
+                curr[j1][j2] = maxi;
+            }
+        }
+        front = curr;
+    }
+
+    return front[0][c-1];
+}

1bd433a65883b0e2c5147910a4832168cd194d9e
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-25 22:09:02 +0530

    Update Partition Equal Subset Sum.cpp

--- SubsequenceDay12/Partition Equal Subset Sum.cpp
+++ SubsequenceDay12/Partition Equal Subset Sum.cpp
@@ -46,7 +46,7 @@ int equalPartition(int N, int arr[])
         // Base condition
         
         for(int i=0;i<N;i++) dp[i][0] = 1;
-        if(arr[0]<sum/2) dp[0][arr[0]] = 1;
+        if(arr[0]<=target) dp[0][arr[0]] = 1;
         
 
         for(int i=1;i<N;i++){
@@ -77,7 +77,7 @@ int equalPartition(int N, int arr[])
         // Base condition
         
         prev[0] = 1;
-        if(arr[0]<sum/2) prev[arr[0]] = 1;
+        if(arr[0]<=target) prev[arr[0]] = 1;
         
 
         for(int i=1;i<N;i++){

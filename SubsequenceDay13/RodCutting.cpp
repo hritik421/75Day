8fafc639ce88ed194c3e6f27f89bc7ca5540dd63
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-29 17:40:21 +0530

    Update RodCutting.cpp

--- SubsequenceDay13/RodCutting.cpp
+++ SubsequenceDay13/RodCutting.cpp
@@ -22,3 +22,26 @@ private:
         vector<vector<int>> dp(n,vector<int>(n+1,-1));
         return f(n-1,n,price,dp);
     }
+
+// Tabulation
+
+ int cutRod(int price[], int n) {
+        //code here
+        vector<vector<int>> dp(n,vector<int>(n+1,0));
+        
+        // Base case
+        for(int i=0;i<=n;i++) dp[0][i] = i*price[0];
+        
+        // Explore all possibilities
+        for(int ind=1;ind<n;ind++){
+            for(int N=0;N<=n;N++){
+                int rodLength = ind+1;
+                int notPick = dp[ind-1][N];
+                int pick = INT_MIN;
+                if(rodLength<=N) pick = price[ind] + dp[ind][N-rodLength];
+                dp[ind][N] = max(pick,notPick);
+            }
+        }
+        
+        return dp[n-1][n];
+    }

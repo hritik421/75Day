298fafc6f981ffbd12447c7ec899a573f2c3ee3c
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-10-29 16:35:05 +0530

    Update Knapsack with Duplicate Items.cpp

--- SubsequenceDay13/Knapsack with Duplicate Items.cpp
+++ SubsequenceDay13/Knapsack with Duplicate Items.cpp
@@ -27,3 +27,28 @@ public:
         return f(N-1,W,val,wt,dp);
     }
 };
+
+// Tabulation
+
+int knapSack(int N, int W, int val[], int wt[])
+{
+    // code here
+    vector<vector<int>> dp(N,vector<int>(W+1,0));
+    
+    // Base Case and make sure to add equals to sign
+    for(int w=0;w<=W;w++){
+        dp[0][w] = ((int)(w/wt[0]))*val[0];
+    }
+    
+    // Explore all possibilities
+    
+    for(int ind=1;ind<N;ind++){
+        for(int w=0;w<=W;w++){
+            int notPick = 0 + dp[ind-1][w];
+            int pick = 0;
+            if(w>=wt[ind]) pick = val[ind] + dp[ind][w-wt[ind]];
+            dp[ind][w]=max(pick,notPick);
+        }
+    }
+    return dp[N-1][W];
+}

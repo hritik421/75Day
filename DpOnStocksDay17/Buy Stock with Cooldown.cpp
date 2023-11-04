c5b04aaad7076c2a4e95eee15c9f21af89d8a1ab
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-11-04 16:41:52 +0530

    Update Buy Stock with Cooldown.cpp

--- DpOnStocksDay17/Buy Stock with Cooldown.cpp
+++ DpOnStocksDay17/Buy Stock with Cooldown.cpp
@@ -19,3 +19,30 @@ long long maximumProfit(vector<long long>&prices, int n) {
         
         return dp[0][1];
     }
+
+// Space Optimized
+
+long long maximumProfit(vector<long long>&prices, int n) {
+        // Code here
+        vector<long long>front1(2,0);
+        vector<long long>front2(2,0);
+        vector<long long>curr(2,0);
+        
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                if(buy){
+                    curr[buy] = max(-prices[ind] + front1[0],
+                                        front1[1]);
+                }
+                else{
+                    curr[buy] = max(prices[ind] + front2[1],
+                                        front1[0]);
+                }
+                front2 = front1;
+                front1 = curr;
+            }
+        }
+        
+        return front1[1];
+    }

39a31055a24411fe56485d7cea1bc639b4e40b9c
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-11-21 22:53:07 +0530

    Create Partition Array for Maximum Sum.cpp

---
+++ DpOnPartitionDay21/Partition Array for Maximum Sum.cpp
@@ -0,0 +1,28 @@
+// Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has 
+// their values changed to become the maximum value of that subarray. Return the largest sum of the given array after partitioning.
+
+// Memoization
+
+    int f(int i, int n, int k, vector<int>& arr, vector<int>& dp){
+        if(i==n) return 0;
+        
+        if(dp[i]!=-1) return dp[i];
+        
+        int len=0;
+        int maxi=INT_MIN, maxAns = INT_MIN;
+        
+        for(int j=i;j<min(n,i+k);j++){
+            len++;
+            maxi = max(maxi,arr[j]);
+            int sum = len*maxi + f(j+1,n,k,arr,dp);
+            maxAns = max(maxAns,sum);
+        }
+        
+        dp[i] = maxAns;
+    }
+    public:
+    int solve(int n, int k, vector<int>& arr){
+        // Code here
+        vector<int> dp(n,-1);
+        return f(0,n,k,arr,dp);
+    }

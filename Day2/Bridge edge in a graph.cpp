b1df02c0242b1a70b15ac225fb6662e40f6af43e
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-11-29 22:42:45 +0530

    Update Bridge edge in a graph.cpp

--- Day2/Bridge edge in a graph.cpp
+++ Day2/Bridge edge in a graph.cpp
@@ -1,3 +1,6 @@
+// Given a Graph of V vertices and E edges and another edge(c - d), the task is to find if the given edge is a Bridge. i.e., 
+// removing the edge disconnects the graph
+
 class Solution
 {
     private:

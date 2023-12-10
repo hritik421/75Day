a3879bf195b145a699879d6741e9f0fa615dcdea
hritik421 <95011875+hritik421@users.noreply.github.com>
2023-12-10 13:21:16 +0530

    Create LowerBoundAndUpperBound.cpp

---
+++ BinarySearchDay23/LowerBoundAndUpperBound.cpp
@@ -0,0 +1,23 @@
+// STL way to use
+int ind = lower_bound(arr.begin(),arr.end(),n) - arr.begin();
+
+
+// implementation
+
+int lowerBound(vector<int> arr, int n, int x) {
+	// Write your code here
+	int low=0,high=n-1,ans=n;
+
+	while(low<=high){
+		int mid = low + (high-low)/2;
+		if(arr[mid]>=x){
+			ans = mid;
+			high=mid-1;
+		}
+		else{
+			low = mid+1;
+		}
+	}
+
+	return ans;
+}

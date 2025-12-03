a089181fc62bae6d13716698270a25a06a04282a
hritik421 <95011875+hritik421@users.noreply.github.com>
2025-12-03 13:21:20 +0530

    Update Minimum number of deletions and insertions.cpp

--- DP/StringSubsequenceDay14/Minimum number of deletions and insertions.cpp
+++ DP/StringSubsequenceDay14/Minimum number of deletions and insertions.cpp
@@ -1,5 +1,8 @@
 // Given two strings str1 and str2. The task is to remove or insert the minimum number of characters from/in str1 so as to transform it into str2. 
 // It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some another point.
+del = len("heap") - 2 = 4 - 2 = 2
+ins = len("pea")  - 2 = 3 - 2 = 1
+Total ops = 3
 
 // Tabulation
 

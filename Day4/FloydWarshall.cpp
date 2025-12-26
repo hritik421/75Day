420f5fbae2d88345a0cb626e0179b5c419801444
hritik421 <95011875+hritik421@users.noreply.github.com>
2025-12-26 23:29:05 +0530

    Update FloydWarshall.cpp

--- Day4/FloydWarshall.cpp
+++ Day4/FloydWarshall.cpp
@@ -1,4 +1,12 @@
 // Watch video again for more clarification
+// Key points:
+// 1. It is for finding shortest path from any source to any destination, it is like multi source
+// 2. In finding shortest we use the adjacency matrix instead of adj list
+// 3. If graph is undirected, while creating adj matrix put same value for both adj[u][v] = adj[v][u] = x;
+// 4. This algo helps in detecting negative weight cycle as well,
+// if cost to reach node itself is negative that means negative weight cycle is there because cost to reach node itself is 0.
+
+// Note: Algo like dijstra or bellman ford are single source shortest path algo
 
 //User function template for C++
 
@@ -11,6 +19,7 @@ class Solution {
 	    
 	    for(int i=0;i<row;i++){
 	        for(int j=0;j<col;j++){
+				// where path is not exist
 	            if(matrix[i][j]==-1) matrix[i][j] = 1e8;
 	        }
 	    }
